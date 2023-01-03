95a2458366a793d9502e8f484d73a61121c0d5df
Bhavjit Chauhan <25716723+bhavjitChauhan@users.noreply.github.com>
2023-01-02 18:11:58 -0800

    Rename `getProfile` to `getUser` Update tests to reflect changes to the method as well.

---
+++ tests/KhanClient#getUser.test.ts
@@ -0,0 +1,31 @@
+import { KhanClient } from '../src/index'
+import User from '../src/User'
+
+describe('KhanClient#getProfile', () => {
+  test('Fetches profile given KAID', async () => {
+    const client = new KhanClient()
+
+    const user = await client.getUser(process.env.KHAN_KAID)
+    expect(user).toBeInstanceOf(User)
+    expect(user?.rawFullUserProfile?.user).not.toBeNull()
+  })
+
+  test('Fetches profile given username', async () => {
+    const client = new KhanClient()
+
+    const user = await client.getUser(process.env.KHAN_USERNAME)
+    expect(user).toBeInstanceOf(User)
+    expect(user?.rawFullUserProfile?.user).not.toBeNull()
+  })
+
+  test('Fetches own profile iff authenticated', async () => {
+    const client = new KhanClient()
+    await expect(client.getUser()).rejects.toThrowError()
+
+    await client.login(process.env.KHAN_IDENTIFIER, process.env.KHAN_PASSWORD)
+    const user = await client.getUser()
+    expect(user).toBeInstanceOf(User)
+    expect(user?.rawFullUserProfile?.user).not.toBeNull()
+    expect(user?.self).toBe(true)
+  })
+})
