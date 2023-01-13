4f0ed54bdb2804edf8b0a648604c49a4ec4741fa
Bhavjit Chauhan <25716723+bhavjitChauhan@users.noreply.github.com>
2023-01-12 19:29:18 -0800

    Polish

--- scripts/build.js
+++ scripts/build.js
@@ -1,58 +1,60 @@
 import esbuild from 'esbuild'
+import chalk from 'chalk';
 
 const FILE_NAME = 'khan-api'
 
-/**
- * @type {import('esbuild').BuildOptions}
- */
+/** @type {import('esbuild').BuildOptions} */
 const base = {
   entryPoints: ['./src/index.ts'],
   bundle: true,
   sourcemap: true,
 }
 
-/**
- * @type {import('esbuild').BuildOptions}
- */
+/** @type {import('esbuild').BuildOptions} */
 const esm = {
   ...base,
   platform: 'neutral',
   target: 'node18',
   format: 'esm',
-  outfile: `dist/esm/${FILE_NAME}.js`,
+  outfile: `esm/${FILE_NAME}.js`,
 }
 
-/**
- * @type {import('esbuild').BuildOptions}
- */
+/** @type {import('esbuild').BuildOptions} */
 const node = {
   ...base,
   platform: 'node',
   target: 'node18',
   format: 'cjs',
-  outfile: `dist/node/${FILE_NAME}.cjs`,
+  outfile: `node/${FILE_NAME}.cjs`,
 }
 
-/**
- * @type {import('esbuild').BuildOptions}
- */
+/** @type {import('esbuild').BuildOptions} */
 const browser = {
   ...base,
   platform: 'browser',
   target: 'es2015',
   globalName: 'KhanAPI',
-  outfile: `dist/${FILE_NAME}.js`,
+  outfile: `${FILE_NAME}.js`,
 }
 
 const configs = [esm, node, browser]
 configs.push(...configs.map(config => ({ ...config, minify: true, outfile: config.outfile.replace(/\.(c?js)$/, '.min.$1') })))
 
-for (const config of configs) {
-  console.log(`Building ${config.outfile}...`)
-  try {
-    await esbuild.build(config)
-  } catch (e) {
-    console.error(`Error building ${config.outfile}: ${e.message}`)
-    process.exit(1)
-  }
-}
+const start = performance.now()
+
+await Promise.allSettled(configs.map(config => {
+  return new Promise(async (resolve, reject) => {
+    const start = performance.now()
+    console.log(chalk.cyan(`Building ${chalk.bold(config.outfile)}...`))
+    try {
+      await esbuild.build(config)
+      console.log(chalk.green(`Built ${chalk.bold(config.outfile)} in ${chalk.bold(Math.ceil(performance.now() - start) + 'ms')}`))
+    } catch (err) {
+      console.error(chalk.red(`Error building ${chalk.bold(config.outfile)}: ${chalk.bold(err.message)}`))
+      reject()
+    }
+    resolve()
+  })
+}))
+
+console.log(chalk.white.bgGreen(` Built files in ${chalk.bold(Math.ceil(performance.now() - start) + 'ms')} `) + '\n')
