00156d7d68830ef846b8be953324ec389714996f
Bhavjit Chauhan <25716723+bhavjitChauhan@users.noreply.github.com>
2023-01-13 00:05:48 -0800

    Create release script

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,10 @@
 /** @type {import('eslint').Linter.Config} */
-
 module.exports = {
   extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:@typescript-eslint/recommended-requiring-type-checking', 'prettier'],
   rules: {
     '@typescript-eslint/no-namespace': 'off',
     '@typescript-eslint/no-non-null-assertion': 'off',
   },
-  overrides: [
-    {
-      files: ['*.ts'],
-      rules: {
-      },
-    },
-  ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     tsconfigRootDir: __dirname,
