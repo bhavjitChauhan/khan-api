774d93c13e0a20a37446b009f289912d0cf6e548
Bhavjit Chauhan <25716723+bhavjitChauhan@users.noreply.github>
2023-09-09 16:31:22 +0000

    Update safelist

--- queries.browser.js
+++ queries.browser.js
@@ -9580,20 +9580,6 @@ fragment gtp_egudFragment on ExamGroupUserData {
     __typename
   }
 }
-
-fragment gtp_essayScoresFragment on EssayScores {
-  areas {
-    translatedTitle
-    essays {
-      examCompletionDate
-      score
-      maxScore
-      __typename
-    }
-    __typename
-  }
-  __typename
-}
 `,
   gtp_getExamGroupMetadata: `query gtp_getExamGroupMetadata($examGroupId: String!) {
   examGroup(examGroupId: $examGroupId) {
