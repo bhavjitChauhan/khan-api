c04524e352bb5a3643ff6ebabfeb40a0b0376ba2
Bhavjit Chauhan <25716723+bhavjitChauhan@users.noreply.github>
2023-12-11 15:31:21 +0000

    Update safelist

--- queries.browser.js
+++ queries.browser.js
@@ -9622,6 +9622,20 @@ fragment gtp_egudFragment on ExamGroupUserData {
     ...gtp_essayScoresFragment
     __typename
   }
+}
+
+fragment gtp_essayScoresFragment on EssayScores {
+  areas {
+    translatedTitle
+    essays {
+      examCompletionDate
+      score
+      maxScore
+      __typename
+    }
+    __typename
+  }
+  __typename
 }`,
   gtp_getExamGroupMetadata: `query gtp_getExamGroupMetadata($examGroupId: String!) {
   examGroup(examGroupId: $examGroupId) {
@@ -17450,74 +17464,6 @@ fragment ProjectRevision on ProjectRevision {
   }
   isEditableByCurrentUser(contentId: $contentId, contentKind: "Exercise")
   isPublishableByCurrentUser(contentId: $contentId, contentKind: "Exercise")
-}
-
-fragment AssessmentItemRevision on AssessmentItemRevision {
-  id
-  contentKind
-  contentId
-  sha
-  creationDate
-  name
-  authorNames
-  itemData
-  itemShapeType
-  perseusApiMajorVersion
-  requiresScreenOrMouse
-  tags
-  __typename
-}
-
-fragment ExerciseRevision on ExerciseRevision {
-  id
-  contentKind
-  contentId
-  sha
-  creationDate
-  authorKey
-  doNotPublish
-  sourceKaLocale
-  sourceLanguage: sourceKaLocale
-  slug
-  name: slug
-  title
-  displayName: title
-  prettyDisplayName: title
-  description
-  descriptionHtml: description
-  customTitleTag
-  customDescriptionTag
-  thumbnailData
-  thumbnailCache
-  sponsored
-  alternateSlugs
-  authorName
-  covers
-  prerequisites
-  relatedContent
-  assessmentItemTags
-  difficultyLevel
-  suggestedCompletionCriteria
-  trackingDocumentUrl
-  problemTypes {
-    name
-    relatedVideos
-    items {
-      id
-      sha
-      perseusApiMajorVersion
-      requiresScreenOrMouse
-      __typename
-    }
-    __typename
-  }
-  assessmentItems {
-    ...AssessmentItemRevision
-    __typename
-  }
-  listed
-  hasLintErrors
-  __typename
 }`,
   getSatAccountSettings: `query getSatAccountSettings($targetKaid: String) {
   target: user(kaid: $targetKaid) {
