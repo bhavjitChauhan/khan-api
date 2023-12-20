34e4854d6d00a47073e3257ba3e22e92fb94e5b0
Bhavjit Chauhan <25716723+bhavjitChauhan@users.noreply.github.com>
2023-12-20 05:15:18 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Khan Proxy
+
+Access Khan Academy's API from anywhere.
+
+## Usage
+
+<!-- ### [Khan API Library](https://github.com/bhavjitChauhan/khan-api)
+
+Since version [0.7.0](https://github.com/bhavjitChauhan/khan-api/releases/tag/v0.7.0), the library will seemlessly use this proxy if not on Khan Academy's domain.
+
+### Manual -->
+
+Replace `www.khanacademy.org` with `khan-proxy.bhavjit.com` in your requests.
+
+See the [examples](examples) directory for working demos.
+
+## Background
+
+Khan Academy's CORS policy does not allow cross-origin requests from other domains. This proxy allows you to make requests to Khan Academy's API from any domain.
---
+++ src/index.ts
@@ -0,0 +1,43 @@
+export interface Env {}
+
+const KHAN_DOMAIN = 'www.khanacademy.org',
+  ALLOWED_PATHNAME = '/api/internal/graphql'
+
+function isRootUrl(url: string) {
+  const { pathname } = new URL(url)
+  return pathname === '/'
+}
+
+function isAllowedProxiedUrl(url: string) {
+  const { hostname, pathname } = new URL(url)
+  return hostname === KHAN_DOMAIN && pathname.startsWith(ALLOWED_PATHNAME)
+}
+
+export default {
+  async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
+    if (isRootUrl(request.url)) return Response.redirect('https://github.com/bhavjitChauhan/khan-api/tree/proxy/')
+
+    const domain = request.headers.get('host')!.replace(/:\d{1,5}$/, ''),
+      domainRegex = new RegExp(`${domain}(?::\\d{1,5})?`)
+
+    const proxiedRequest = new Request(request.url.replace(domainRegex, KHAN_DOMAIN), {
+      method: request.method,
+      headers: request.headers,
+      body: request.body,
+    })
+    if (!isAllowedProxiedUrl(proxiedRequest.url))
+      return new Response(`Only \`${request.headers.get('host')}${ALLOWED_PATHNAME}\` is allowed`, { status: 403 })
+
+    const proxiedResponse = await fetch(proxiedRequest)
+
+    const modifiedHeaders = new Headers(proxiedResponse.headers)
+    modifiedHeaders.set('Access-Control-Allow-Origin', request.headers.get('origin') ?? '*')
+    const modifiedResponse = new Response(proxiedResponse.body, {
+      status: proxiedResponse.status,
+      statusText: proxiedResponse.statusText,
+      headers: modifiedHeaders,
+    })
+
+    return modifiedResponse
+  },
+}
