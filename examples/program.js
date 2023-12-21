7ec4456d48e4cb0fa1498554057b0ec298882ca8
Bhavjit Chauhan <25716723+bhavjitChauhan@users.noreply.github.com>
2023-12-21 01:44:23 -0800

    Create examples directory

---
+++ examples/program.js
@@ -0,0 +1,7 @@
+const { Client } = require("@bhavjit/khan-api")
+
+const client = new Client()
+
+// Get info about a program
+const program = await client.getProgram(6058668928843776)
+console.log(program)
--- examples/user.js
+++ examples/user.js
@@ -5,7 +5,3 @@ const client = new Client()
 // Get info about a user
 const user = await client.getUser('bhavjitChauhan')
 console.log(user)
-
-// Get info about a program
-const program = await client.getProgram(6058668928843776)
-console.log(program)
