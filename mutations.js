faa949c6da99ffd0037c02840ef8425bfc33947b
Bhavjit Chauhan <102578407+bhavjitAlt@users.noreply.github.com>
2024-04-26 03:30:14 +0000

    Update safelist

--- mutations.js
+++ mutations.js
@@ -11696,7 +11696,7 @@ fragment ProjectRevision on ProjectRevision {
   }
 }`,
   EnableDisableChildrenAIGuide: `mutation EnableDisableChildrenAIGuide($enableKAIDs: [String!]!, $disableKAIDs: [String!]!) {
-  aiGuideActivateChildAccounts(childKAIDs: $enableKAIDs) {
+  aiGuideDeactivateChildAccounts(childKAIDs: $disableKAIDs) {
     errors {
       code
       debugMessage
@@ -11705,7 +11705,7 @@ fragment ProjectRevision on ProjectRevision {
     }
     __typename
   }
-  aiGuideDeactivateChildAccounts(childKAIDs: $disableKAIDs) {
+  aiGuideActivateChildAccounts(childKAIDs: $enableKAIDs) {
     errors {
       code
       debugMessage
