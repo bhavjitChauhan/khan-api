1fdf6f195e49a47cd01016644746f4a3ca0e45c6
Bhavjit Chauhan <102578407+bhavjitAlt@users.noreply.github.com>
2024-06-04 22:30:13 +0000

    Update safelist

--- hashes.js
+++ hashes.js
@@ -696,4 +696,5 @@ export default {
   devadminEssayHistory: 205459217,
   devadminEssayAuthor: 3140435347,
   devadminWritingCoachTime: 3823115589,
+  listAIGuideConversationTestCases: 1761668825,
 }
