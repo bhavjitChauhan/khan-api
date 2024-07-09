1ad5aaf79c8c09f69b8434a0dd0d9872e966c56e
Bhavjit Chauhan <102578407+bhavjitAlt@users.noreply.github.com>
2024-07-09 20:30:16 +0000

    Update safelist

--- hashes.js
+++ hashes.js
@@ -695,7 +695,7 @@ export default {
   districtById: 728152285,
   devadminEssayHistory: 2183177348,
   devadminEssayAuthor: 3140435347,
-  devadminWritingCoachTime: 344083725,
+  devadminWritingCoachTime: 1031216170,
   listAIGuideConversationTestCases: 981204477,
   fetchAllLegalDocs: 2516438376,
   getTeacherTools: 4232440853,
@@ -721,4 +721,6 @@ export default {
   previewLearnerParentAIGuideActivatedEmail: 961900096,
   interimAssessmentCheckQuery: 1693632382,
   StreakQuery: 3728201606,
+  essaySessionHistory: 1277525593,
+  getCoachAssignmentReport: 207191496,
 }
