b1353bf4a4b0bc449b41524ec0eb0ab440f3ffdf
Bhavjit Chauhan <102578407+bhavjitAlt@users.noreply.github.com>
2024-11-05 10:31:43 +0000

    Update safelist

--- queries.browser.js
+++ queries.browser.js
@@ -9770,6 +9770,44 @@ fragment CommonUserInfoFragment on User {
     ...gtp_checkpointFragment
     __typename
   }
+}
+
+fragment gtp_checkpointFragment on Checkpoint {
+  id
+  stages {
+    numCreditedTasks
+    creditedTaskIds
+    goalTasks
+    stageIndex
+    stageDisplayNumber
+    startedAt
+    completedAt
+    incomingLevels {
+      name
+      level
+      __typename
+    }
+    focusAreas {
+      skillTitle
+      areaId
+      areaTitle
+      __typename
+    }
+    __typename
+  }
+  checkpointIndex
+  startedAt
+  completedAt
+  drillMode
+  isComplete
+  removedFromSchedule
+  hasDonePracticeTasks
+  numStages
+  canCreateNewStage
+  hasDonePracticeTasks
+  miniSectionStages
+  tmsTaskIds
+  __typename
 }`,
   gtp_getDescriptors: `query gtp_getDescriptors($examId: String!, $checkpointStr: String) {
   descriptorList(examId: $examId, requestedCheckpoint: $checkpointStr) {
@@ -9784,38 +9822,6 @@ fragment CommonUserInfoFragment on User {
     ...gtp_egudFragment
     __typename
   }
-}
-
-fragment gtp_egudFragment on ExamGroupUserData {
-  id
-  practiceDaysInfo {
-    day
-    hour
-    length
-    minute
-    __typename
-  }
-  selectedCutoffIdentifiers
-  selectedExams
-  initialSelectedExams
-  dailyActivityHistory {
-    date
-    activityHistory {
-      examsCompleted
-      problemsDone
-      secondsSpent
-      status
-      cta {
-        type
-        minutes
-        __typename
-      }
-      __typename
-    }
-    __typename
-  }
-  extendedTimeMultiplier
-  __typename
 }`,
   gtp_getEGUDViaExam: `query gtp_getEGUDViaExam($examId: String!) {
   exam(examId: $examId) {
@@ -9830,38 +9836,6 @@ fragment gtp_egudFragment on ExamGroupUserData {
     }
     __typename
   }
-}
-
-fragment gtp_egudFragment on ExamGroupUserData {
-  id
-  practiceDaysInfo {
-    day
-    hour
-    length
-    minute
-    __typename
-  }
-  selectedCutoffIdentifiers
-  selectedExams
-  initialSelectedExams
-  dailyActivityHistory {
-    date
-    activityHistory {
-      examsCompleted
-      problemsDone
-      secondsSpent
-      status
-      cta {
-        type
-        minutes
-        __typename
-      }
-      __typename
-    }
-    __typename
-  }
-  extendedTimeMultiplier
-  __typename
 }`,
   gtp_getEmailRemindersData: `query gtp_getEmailRemindersData($listName: String!) {
   user {
@@ -9907,60 +9881,12 @@ fragment gtp_essayScoresFragment on EssayScores {
     }
     __typename
   }
-}
-
-fragment gtp_examGroupMetadataFragment on ExamGroupMetadata {
-  id
-  testMaker
-  kaRelationshipToTestCopy
-  testMakerLogoImg
-  registrationUrl
-  __typename
 }`,
   gtp_getExamMetadata: `query gtp_getExamMetadata($examId: String!) {
   metadata(examId: $examId) {
     ...gtp_examMetadataFragment
     __typename
   }
-}
-
-fragment gtp_examMetadataFragment on ExamMetadata {
-  id
-  accommodationsUrl
-  accommodationsCopy
-  accommodationsOptions
-  numAvailableTests
-  offerFullTestDiagnostics
-  goalScoreContext
-  goalScoreResources {
-    url
-    description
-    __typename
-  }
-  skillLevelLabels {
-    skillLevel
-    label
-    __typename
-  }
-  examGroupMetadata {
-    examGroupId
-    exams {
-      examId
-      __typename
-    }
-    __typename
-  }
-  subExamMode
-  isPassFail
-  cutoffScoreSelectorContext
-  cutoffScoreDefaultText
-  passFailScoreCutoffs {
-    label
-    identifier
-    cutoffScoreThreshold
-    __typename
-  }
-  __typename
 }`,
   gtp_getExamOnboardingLessons: `query gtp_getExamOnboardingLessons($examId: String!) {
   exam(examId: $examId) {
@@ -10171,131 +10097,6 @@ fragment gtp_examMetadataFragment on ExamMetadata {
     ...gtp_taskFragment
     __typename
   }
-}
-
-fragment gtp_taskFragment on Task {
-  id
-  kaid
-  examId
-  taskType
-  secondsTaken
-  taskDurationSeconds
-  translatedTitle
-  creationDatetime
-  startDatetime
-  completed
-  receivedCredit
-  completionDatetime
-  stage
-  checkpoint
-  taskContent {
-    concepts {
-      item {
-        conceptId
-        translatedTitle
-        __typename
-      }
-      questions {
-        conceptId
-        translatedTitle
-        __typename
-      }
-      __typename
-    }
-    id
-    itemData
-    itemShapeType
-    skills {
-      item {
-        areaId
-        skillId
-        skillContentId
-        translatedTitle
-        __typename
-      }
-      questions {
-        areaId
-        skillId
-        skillContentId
-        translatedTitle
-        __typename
-      }
-      __typename
-    }
-    gradingMetadata {
-      instructions
-      promptTitle
-      responseTitle
-      minScore
-      maxScore
-      rubric {
-        article {
-          id
-          perseusContent
-          __typename
-        }
-        __typename
-      }
-      scoreExamples {
-        score
-        article {
-          id
-          perseusContent
-          __typename
-        }
-        __typename
-      }
-      __typename
-    }
-    __typename
-  }
-  taskState
-  taskStateHash
-  exerciseName
-  itemIds
-  areaId
-  areaTitle
-  ... on SkillTask {
-    skillId
-    skill {
-      description
-      __typename
-    }
-    level
-    incomingSkillLevelLabel {
-      label
-      skillLevel
-      __typename
-    }
-    outgoingSkillLevelLabel {
-      label
-      skillLevel
-      __typename
-    }
-    __typename
-  }
-  ... on TmsTask {
-    directions
-    extendedTaskState
-    startExtendedTimeDt
-    __typename
-  }
-  ... on TestSectionTask {
-    directions
-    __typename
-  }
-  ... on ExpressDiagnosticTask {
-    directions
-    skillLevels {
-      skillName
-      minLevel
-      maxLevel
-      level
-      __typename
-    }
-    __typename
-  }
-  __typename
 }`,
   gtp_getTestPrepHeaderInfo: `query gtp_getTestPrepHeaderInfo($examId: String!) {
   exam(examId: $examId) {
@@ -10319,131 +10120,6 @@ fragment gtp_taskFragment on Task {
     ...gtp_taskFragment
     __typename
   }
-}
-
-fragment gtp_taskFragment on Task {
-  id
-  kaid
-  examId
-  taskType
-  secondsTaken
-  taskDurationSeconds
-  translatedTitle
-  creationDatetime
-  startDatetime
-  completed
-  receivedCredit
-  completionDatetime
-  stage
-  checkpoint
-  taskContent {
-    concepts {
-      item {
-        conceptId
-        translatedTitle
-        __typename
-      }
-      questions {
-        conceptId
-        translatedTitle
-        __typename
-      }
-      __typename
-    }
-    id
-    itemData
-    itemShapeType
-    skills {
-      item {
-        areaId
-        skillId
-        skillContentId
-        translatedTitle
-        __typename
-      }
-      questions {
-        areaId
-        skillId
-        skillContentId
-        translatedTitle
-        __typename
-      }
-      __typename
-    }
-    gradingMetadata {
-      instructions
-      promptTitle
-      responseTitle
-      minScore
-      maxScore
-      rubric {
-        article {
-          id
-          perseusContent
-          __typename
-        }
-        __typename
-      }
-      scoreExamples {
-        score
-        article {
-          id
-          perseusContent
-          __typename
-        }
-        __typename
-      }
-      __typename
-    }
-    __typename
-  }
-  taskState
-  taskStateHash
-  exerciseName
-  itemIds
-  areaId
-  areaTitle
-  ... on SkillTask {
-    skillId
-    skill {
-      description
-      __typename
-    }
-    level
-    incomingSkillLevelLabel {
-      label
-      skillLevel
-      __typename
-    }
-    outgoingSkillLevelLabel {
-      label
-      skillLevel
-      __typename
-    }
-    __typename
-  }
-  ... on TmsTask {
-    directions
-    extendedTaskState
-    startExtendedTimeDt
-    __typename
-  }
-  ... on TestSectionTask {
-    directions
-    __typename
-  }
-  ... on ExpressDiagnosticTask {
-    directions
-    skillLevels {
-      skillName
-      minLevel
-      maxLevel
-      level
-      __typename
-    }
-    __typename
-  }
-  __typename
 }`,
   gtp_getTPUD: `query gtp_getTPUD($examId: String!) {
   checkpoints(examId: $examId) {
@@ -10472,6 +10148,109 @@ fragment gtp_taskFragment on Task {
     ...gtp_tpudFragment
     __typename
   }
+}
+
+fragment gtp_checkpointFragment on Checkpoint {
+  id
+  stages {
+    numCreditedTasks
+    creditedTaskIds
+    goalTasks
+    stageIndex
+    stageDisplayNumber
+    startedAt
+    completedAt
+    incomingLevels {
+      name
+      level
+      __typename
+    }
+    focusAreas {
+      skillTitle
+      areaId
+      areaTitle
+      __typename
+    }
+    __typename
+  }
+  checkpointIndex
+  startedAt
+  completedAt
+  drillMode
+  isComplete
+  removedFromSchedule
+  hasDonePracticeTasks
+  numStages
+  canCreateNewStage
+  hasDonePracticeTasks
+  miniSectionStages
+  tmsTaskIds
+  __typename
+}
+
+fragment gtp_practiceTestFragment on PracticeTest {
+  id
+  practiceTestId
+  approxTestMins
+  testTitle
+  directions
+  formCode
+  hasStarted
+  completionStatus
+  completedAt
+  subScores {
+    name
+    score
+    __typename
+  }
+  sections {
+    sectionId
+    taskId
+    exerciseName
+    isScored
+    sectionTitle
+    numCorrect
+    numTotal
+    durationSeconds
+    breakDurationSeconds
+    hasUserGrading
+    completed
+    userProvidedScores {
+      score
+      minScore
+      maxScore
+      __typename
+    }
+    __typename
+  }
+  __typename
+}
+
+fragment gtp_tpudFragment on TestPrepUserData {
+  id
+  examId
+  currentStage
+  currentCheckpoint
+  targetScore
+  diagnosticsStates {
+    type
+    state
+    __typename
+  }
+  onboardingState
+  hasSeededSkillLevels
+  hasUnlockedDrillMode
+  scoreInfo {
+    minScore
+    maxScore
+    __typename
+  }
+  schedule {
+    examDate
+    practiceTestDates
+    __typename
+  }
+  __typename
 }`,
   gtp_onboardingStatus: `query gtp_onboardingStatus($examGroupId: String!) {
   egud(examGroupId: $examGroupId) {
@@ -17485,26 +17264,6 @@ fragment ChallengeRevision on ChallengeRevision {
   }
   isEditableByCurrentUser(contentId: $contentId, contentKind: "Project")
   isPublishableByCurrentUser(contentId: $contentId, contentKind: "Project")
-}
-
-fragment ProjectRevision on ProjectRevision {
-  id
-  contentId
-  contentKind
-  sha
-  doNotPublish
-  slug
-  listed
-  title
-  description
-  projectEval
-  authorName
-  code
-  codeFormat
-  customTitleTag
-  customDescriptionTag
-  sourceKaLocale
-  __typename
 }`,
   getAdminsForDistrict: `query getAdminsForDistrict($districtID: String!) {
   getAdminsForDistrict(districtID: $districtID) {
@@ -17813,80 +17572,6 @@ fragment ProjectRevision on ProjectRevision {
     nickname
     __typename
   }
-}
-
-fragment AssessmentItemRevision on AssessmentItemRevision {
-  id
-  contentKind
-  contentId
-  sha
-  creationDate
-  name
-  authorNames
-  itemData
-  itemShapeType
-  perseusApiMajorVersion
-  requiresScreenOrMouse
-  aiGenerated
-  tags
-  __typename
-}
-
-fragment ExerciseRevision on ExerciseRevision {
-  id
-  contentKind
-  contentId
-  sha
-  creationDate
-  authorKey
-  doNotPublish
-  sourceKaLocale
-  sourceLanguage: sourceKaLocale
-  slug
-  name: slug
-  title
-  displayName: title
-  prettyDisplayName: title
-  description
-  descriptionHtml: description
-  customTitleTag
-  customDescriptionTag
-  thumbnailData
-  thumbnailCache
-  sponsored
-  alternateSlugs
-  authorName
-  covers
-  prerequisites
-  relatedContent
-  assessmentItemTags
-  difficultyLevel
-  suggestedCompletionCriteria
-  trackingDocumentUrl
-  problemTypeKind
-  interimAssessmentType
-  numQuestionsPerProblemType
-  problemTypes {
-    contentId
-    name
-    relatedVideos
-    items {
-      id
-      sha
-      perseusApiMajorVersion
-      requiresScreenOrMouse
-      aiGenerated
-      __typename
-    }
-    __typename
-  }
-  assessmentItems {
-    ...AssessmentItemRevision
-    __typename
-  }
-  listed
-  hasLintErrors
-  __typename
 }`,
   getSatAccountSettings: `query getSatAccountSettings($targetKaid: String) {
   target: user(kaid: $targetKaid) {
