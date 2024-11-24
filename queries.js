dc68b109f985ac1f70d3162bf87f37f0ce754e4e
Bhavjit Chauhan <102578407+bhavjitAlt@users.noreply.github.com>
2024-11-24 08:31:41 +0000

    Update safelist

--- queries.js
+++ queries.js
@@ -329,35 +329,6 @@ fragment ActivitySessionSkillLevels on SkillLevelChange {
     ...ArticleRevision
     __typename
   }
-}
-
-fragment ArticleRevision on ArticleRevision {
-  id
-  contentId
-  contentKind
-  creationDate
-  sha
-  authorKey
-  customDescriptionTag
-  customTitleTag
-  description
-  descriptionHtml: description
-  doNotPublish
-  sourceKaLocale
-  sourceLanguage: sourceKaLocale
-  slug
-  readableId: slug
-  title
-  sponsored
-  thumbnailData
-  thumbnailCache
-  alternateSlugs
-  assessmentItemTags
-  authorNames
-  clarificationsEnabled
-  perseusContent
-  listed
-  __typename
 }`,
   articleEditorRedirectQuery: `query articleEditorRedirectQuery($contentId: String!) {
   articleRevisionById(id: $contentId) {
@@ -17112,6 +17083,30 @@ fragment contentSearchLearnableContent on LearnableContent {
   }
   isEditableByCurrentUser(contentId: $contentId, contentKind: "Challenge")
   isPublishableByCurrentUser(contentId: $contentId, contentKind: "Challenge")
+}
+
+fragment ChallengeRevision on ChallengeRevision {
+  id
+  contentId
+  contentKind
+  sha
+  doNotPublish
+  slug
+  listed
+  title
+  description
+  authorName
+  code
+  codeFormat
+  tests
+  testsFormat
+  height
+  width
+  userAuthoredContentType
+  customTitleTag
+  customDescriptionTag
+  sourceKaLocale
+  __typename
 }`,
   projectEditorQuery: `query projectEditorQuery($contentId: String!) {
   projectRevisionById(id: $contentId) {
