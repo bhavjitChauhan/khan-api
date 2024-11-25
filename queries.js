c799454e39cba5c8f190df9efd376597723f52ef
Bhavjit Chauhan <102578407+bhavjitAlt@users.noreply.github.com>
2024-11-25 13:31:40 +0000

    Update safelist

--- queries.js
+++ queries.js
@@ -329,35 +329,6 @@ fragment ActivitySessionSkillLevels on SkillLevelChange {
     ...ArticleRevision
     __typename
   }
-}
-
-fragment ArticleRevision on ArticleRevision {
-  id
-  contentId
-  contentKind
-  creationDate
-  sha
-  authorKey
-  customDescriptionTag
-  customTitleTag
-  description
-  descriptionHtml: description
-  doNotPublish
-  sourceKaLocale
-  sourceLanguage: sourceKaLocale
-  slug
-  readableId: slug
-  title
-  sponsored
-  thumbnailData
-  thumbnailCache
-  alternateSlugs
-  assessmentItemTags
-  authorNames
-  clarificationsEnabled
-  perseusContent
-  listed
-  __typename
 }`,
   articleEditorRedirectQuery: `query articleEditorRedirectQuery($contentId: String!) {
   articleRevisionById(id: $contentId) {
@@ -17112,6 +17083,30 @@ fragment contentSearchLearnableContent on LearnableContent {
   }
   isEditableByCurrentUser(contentId: $contentId, contentKind: "Challenge")
   isPublishableByCurrentUser(contentId: $contentId, contentKind: "Challenge")
+}
+
+fragment ChallengeRevision on ChallengeRevision {
+  id
+  contentId
+  contentKind
+  sha
+  doNotPublish
+  slug
+  listed
+  title
+  description
+  authorName
+  code
+  codeFormat
+  tests
+  testsFormat
+  height
+  width
+  userAuthoredContentType
+  customTitleTag
+  customDescriptionTag
+  sourceKaLocale
+  __typename
 }`,
   projectEditorQuery: `query projectEditorQuery($contentId: String!) {
   projectRevisionById(id: $contentId) {
@@ -17438,6 +17433,80 @@ fragment contentSearchLearnableContent on LearnableContent {
     nickname
     __typename
   }
+}
+
+fragment AssessmentItemRevision on AssessmentItemRevision {
+  id
+  contentKind
+  contentId
+  sha
+  creationDate
+  name
+  authorNames
+  itemData
+  itemShapeType
+  perseusApiMajorVersion
+  requiresScreenOrMouse
+  aiGenerated
+  tags
+  __typename
+}
+
+fragment ExerciseRevision on ExerciseRevision {
+  id
+  contentKind
+  contentId
+  sha
+  creationDate
+  authorKey
+  doNotPublish
+  sourceKaLocale
+  sourceLanguage: sourceKaLocale
+  slug
+  name: slug
+  title
+  displayName: title
+  prettyDisplayName: title
+  description
+  descriptionHtml: description
+  customTitleTag
+  customDescriptionTag
+  thumbnailData
+  thumbnailCache
+  sponsored
+  alternateSlugs
+  authorName
+  covers
+  prerequisites
+  relatedContent
+  assessmentItemTags
+  difficultyLevel
+  suggestedCompletionCriteria
+  trackingDocumentUrl
+  problemTypeKind
+  interimAssessmentType
+  numQuestionsPerProblemType
+  problemTypes {
+    contentId
+    name
+    relatedVideos
+    items {
+      id
+      sha
+      perseusApiMajorVersion
+      requiresScreenOrMouse
+      aiGenerated
+      __typename
+    }
+    __typename
+  }
+  assessmentItems {
+    ...AssessmentItemRevision
+    __typename
+  }
+  listed
+  hasLintErrors
+  __typename
 }`,
   getSatAccountSettings: `query getSatAccountSettings($targetKaid: String) {
   target: user(kaid: $targetKaid) {
@@ -21002,38 +21071,6 @@ fragment ExerciseContentFields on LearnableContent {
     }
     __typename
   }
-}
-
-fragment AssignmentFields on Assignment {
-  id
-  studentKaids
-  isDraft
-  assignmentStatus
-  subjectSlug
-  numStudentsCompleted
-  assignedDate
-  startDate
-  dueDate
-  contentDescriptors
-  domainId
-  courseId
-  unitId
-  lessonId
-  contents {
-    id
-    title: translatedTitle
-    kind
-    defaultUrlPath
-    __typename
-  }
-  exerciseConfig {
-    itemPickerStrategy
-    __typename
-  }
-  title
-  instructions
-  configuredActivityInputs
-  __typename
 }`,
   essaySnapshotPatches: `query essaySnapshotPatches($essaySessionId: String!, $snapshotIndex: Int!) {
   essaySession(essaySessionID: $essaySessionId) {
