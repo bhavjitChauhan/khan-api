166da2b23b75d420f25ea69583a9be164cff4884
Bhavjit Chauhan <102578407+bhavjitAlt@users.noreply.github.com>
2024-11-28 09:51:02 +0000

    Update safelist

--- queries.js
+++ queries.js
@@ -17112,30 +17112,6 @@ fragment contentSearchLearnableContent on LearnableContent {
   }
   isEditableByCurrentUser(contentId: $contentId, contentKind: "Challenge")
   isPublishableByCurrentUser(contentId: $contentId, contentKind: "Challenge")
-}
-
-fragment ChallengeRevision on ChallengeRevision {
-  id
-  contentId
-  contentKind
-  sha
-  doNotPublish
-  slug
-  listed
-  title
-  description
-  authorName
-  code
-  codeFormat
-  tests
-  testsFormat
-  height
-  width
-  userAuthoredContentType
-  customTitleTag
-  customDescriptionTag
-  sourceKaLocale
-  __typename
 }`,
   projectEditorQuery: `query projectEditorQuery($contentId: String!) {
   projectRevisionById(id: $contentId) {
@@ -17149,26 +17125,6 @@ fragment ChallengeRevision on ChallengeRevision {
   }
   isEditableByCurrentUser(contentId: $contentId, contentKind: "Project")
   isPublishableByCurrentUser(contentId: $contentId, contentKind: "Project")
-}
-
-fragment ProjectRevision on ProjectRevision {
-  id
-  contentId
-  contentKind
-  sha
-  doNotPublish
-  slug
-  listed
-  title
-  description
-  projectEval
-  authorName
-  code
-  codeFormat
-  customTitleTag
-  customDescriptionTag
-  sourceKaLocale
-  __typename
 }`,
   getAdminsForDistrict: `query getAdminsForDistrict($districtID: String!) {
   getAdminsForDistrict(districtID: $districtID) {
