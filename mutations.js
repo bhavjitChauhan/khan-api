c5da5419791c2ec18f24fac6a05084ea5b4d8165
Bhavjit Chauhan <102578407+bhavjitAlt@users.noreply.github.com>
2025-01-16 08:52:18 +0000

    Update safelist

--- mutations.js
+++ mutations.js
@@ -3944,19 +3944,6 @@ fragment UnlinkedStudentData on UnsuccessfullyLinkedGoogleClassStudent {
     }
     __typename
   }
-}
-
-fragment Program on Program {
-  id
-  latestRevision {
-    id
-    code
-    __typename
-  }
-  title
-  url
-  userAuthoredContentType
-  __typename
 }`,
   createStudentAccounts: `mutation createStudentAccounts($studentListKey: String, $students: [CreateStudentsInput]!) {
   createStudents(students: $students, studentListKey: $studentListKey) {
@@ -8533,19 +8520,6 @@ fragment entry on TeamPageEntryForEditing {
     }
     __typename
   }
-}
-
-fragment Program on Program {
-  id
-  latestRevision {
-    id
-    code
-    __typename
-  }
-  title
-  url
-  userAuthoredContentType
-  __typename
 }`,
   startMasteryTowerSession: `mutation startMasteryTowerSession($classDescriptor: String!) {
   startMasteryTowerSession(classDescriptor: $classDescriptor) {
